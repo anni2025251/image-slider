4c8692c78926f452b51c37e9c89f1f0d0dbdffa1
anni2025251 <ay1737646@gmail.com>
2025-04-24 17:05:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const track = document.getElementById('sliderTrack');
+
+// for (let i = 1; i <= 100; i++) {
+//   const img = document.createElement('img');
+//   img.src = `https://via.placeholder.com/180x120?text=Image+${i}`;
+//   track.appendChild(img);
+// }
+
+// // Clone for smooth looping
+// for (let i = 1; i <= 100; i++) {
+//   const img = document.createElement('img');
+//   img.src = `https://via.placeholder.com/180x120?text=Image+${i}`;
+//   track.appendChild(img);
+// }
